a4486e203a279b22a7b903b94dd9b5747598da7f
Nicolas Fraisse <ni.fraisse@gmail.com>
2022-12-22 23:15:09 -0500

    Add static props for seo

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Poutine Mania</title>
         <meta name="referrer" content="no-referrer" />
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
--- pages/restaurants/[id]/index.js
+++ pages/restaurants/[id]/index.js
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import Spinner from "components/Spinner";
 import RestaurantReviews from "components/page-layouts/RestaurantReviews";
 import RestaurantHeader from "components/RestaurantHeader";
+import { ObjectId } from "mongodb";
+
 import {
   Edit,
   ExternalLink,
@@ -22,8 +24,10 @@ import { ToggleSwitch } from "../../../components/controls/ToggleSwitch";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useCurrentUser } from "lib/useCurrentUser";
+import Head from "next/head";
+import { connectToDatabase } from "../../../lib/db";
 
-const Index = () => {
+const Index = ({ SEO }) => {
   const { query, reload, push } = useRouter();
   const { data: restaurant, loading } = useGet(`/api/restaurants/${query.id}`, {
     skip: !query.id,
@@ -42,6 +46,8 @@ const Index = () => {
       }
     });
   }, []);
+
+  console.log("test", SEO);
 
   const handleApprove = async (id, approved) => {
     await axios
@@ -65,67 +71,104 @@ const Index = () => {
   };
 
   if (!restaurant || loading) return <Spinner />;
-  return (
-    <div className="bg-[#fafafa] min-h-screen-minus-navbar">
-      <RestaurantHeader restaurant={restaurant} />
-      <div className="p-4 xl:p-6 flex flex-col-reverse lg:flex-row">
-        <div className="lg:basis-2/3 lg:max-w-2/3">
-          {!restaurant.approved && (
-            <div className="p-2 lg:p-5 sm:w-auto bg-yellow-50 shadow-md rounded-lg text-sm flex mb-4">
-              <Info className="inline-block min-w-8 mt-1 mr-2" size={20} />
-              <p className="flex-shrink">
-                Ce restaurant est en cours de vérification par un membre de
-                notre équipe. En attendant, vous pouvez écrire un avis sur leur
-                poutine, il sera visible par le reste de la communauté aussitôt
-                que le restaurant est approuvé!
-              </p>
-            </div>
-          )}
-          <RestaurantReviews restaurant={restaurant} />
-        </div>
-        <div className="lg:w-1/3 lg:sticky lg:top-4 lg:h-full lg:ml-4 xl:ml-6 block sm:flex flex-row-reverse items-center lg:block">
-          {currentUser?.isAdmin && (
-            <div className="bg-white shadow-md rounded-lg text-sm p-3 xl:p-5 mb-4 text-center text-gray-800 w-auto sm:w-1/2 lg:w-auto flex items-center justify-between">
-              <div className="flex">
-                <ToggleSwitch
-                  onChange={() =>
-                    handleApprove(restaurant._id, !restaurant.approved)
-                  }
-                  checked={restaurant.approved}
-                />
-                <span className="ml-2">Approved</span>
-              </div>
-              <div>
-                <button
-                  className="p-1 bg-gray-200 rounded shadow hover:bg-gray-100 mx-2"
-                  onClick={() => push(`/restaurants/${restaurant._id}/edit`)}
-                >
-                  <Edit size={20} />
-                </button>
-                <button
-                  className="p-1 bg-gray-200 rounded shadow hover:bg-gray-100 mx-2"
-                  onClick={() => handleDelete(restaurant)}
-                >
-                  <Trash size={20} />
-                </button>
-              </div>
-            </div>
-          )}
-          <div className="bg-white shadow-md rounded-lg text-sm p-3 xl:p-5 mb-4 text-center text-gray-800 w-auto sm:w-1/2 lg:w-auto">
-            <ReviewOverview restaurant={restaurant} />
-          </div>
 
-          <div className="bg-white shadow-md rounded-lg text-xs xl:text-sm p-4 text-gray-800 w-auto sm:w-1/2 lg:w-auto sm:mr-4 lg:mr-0 mb-4 lg:mb-0">
-            <RestaurantInfo
-              showMap={showMap}
-              setShowMap={setShowMap}
-              restaurant={restaurant}
-            />
+  return (
+    <>
+      <Head>
+        <title>{SEO.restaurantName} - Poutine Mania</title>
+        <meta name="description" content="ok" />
+      </Head>
+      <div className="bg-[#fafafa] min-h-screen-minus-navbar">
+        <RestaurantHeader restaurant={restaurant} />
+        <div className="p-4 xl:p-6 flex flex-col-reverse lg:flex-row">
+          <div className="lg:basis-2/3 lg:max-w-2/3">
+            {!restaurant.approved && (
+              <div className="p-2 lg:p-5 sm:w-auto bg-yellow-50 shadow-md rounded-lg text-sm flex mb-4">
+                <Info className="inline-block min-w-8 mt-1 mr-2" size={20} />
+                <p className="flex-shrink">
+                  Ce restaurant est en cours de vérification par un membre de
+                  notre équipe. En attendant, vous pouvez écrire un avis sur
+                  leur poutine, il sera visible par le reste de la communauté
+                  aussitôt que le restaurant est approuvé!
+                </p>
+              </div>
+            )}
+            <RestaurantReviews restaurant={restaurant} />
+          </div>
+          <div className="lg:w-1/3 lg:sticky lg:top-4 lg:h-full lg:ml-4 xl:ml-6 block sm:flex flex-row-reverse items-center lg:block">
+            {currentUser?.isAdmin && (
+              <div className="bg-white shadow-md rounded-lg text-sm p-3 xl:p-5 mb-4 text-center text-gray-800 w-auto sm:w-1/2 lg:w-auto flex items-center justify-between">
+                <div className="flex">
+                  <ToggleSwitch
+                    onChange={() =>
+                      handleApprove(restaurant._id, !restaurant.approved)
+                    }
+                    checked={restaurant.approved}
+                  />
+                  <span className="ml-2">Approved</span>
+                </div>
+                <div>
+                  <button
+                    className="p-1 bg-gray-200 rounded shadow hover:bg-gray-100 mx-2"
+                    onClick={() => push(`/restaurants/${restaurant._id}/edit`)}
+                  >
+                    <Edit size={20} />
+                  </button>
+                  <button
+                    className="p-1 bg-gray-200 rounded shadow hover:bg-gray-100 mx-2"
+                    onClick={() => handleDelete(restaurant)}
+                  >
+                    <Trash size={20} />
+                  </button>
+                </div>
+              </div>
+            )}
+            <div className="bg-white shadow-md rounded-lg text-sm p-3 xl:p-5 mb-4 text-center text-gray-800 w-auto sm:w-1/2 lg:w-auto">
+              <ReviewOverview restaurant={restaurant} />
+            </div>
+
+            <div className="bg-white shadow-md rounded-lg text-xs xl:text-sm p-4 text-gray-800 w-auto sm:w-1/2 lg:w-auto sm:mr-4 lg:mr-0 mb-4 lg:mb-0">
+              <RestaurantInfo
+                showMap={showMap}
+                setShowMap={setShowMap}
+                restaurant={restaurant}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
+
+export async function getStaticPaths() {
+  const client = await connectToDatabase();
+  const db = await client.db();
+
+  const restaurants = await db
+    .collection("restaurants")
+    .find({ approved: true })
+    .toArray();
+
+  return {
+    paths: restaurants.map((r) => `/restaurants/${r._id}`),
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const client = await connectToDatabase();
+  const db = await client.db();
+
+  const restaurant = await db
+    .collection("restaurants")
+    .findOne({ _id: ObjectId(params.id) });
+
+  return {
+    props: {
+      SEO: { restaurantName: restaurant.name },
+    },
+  };
+}
 
 export default Index;
